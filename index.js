c2b9d4822a2aec6494a0a8668bdb3d6f73395109
jkaho <72959522+jkaho@users.noreply.github.com>
2021-07-30 09:52:17 +1000

    -

---
+++ index.js
@@ -0,0 +1,33 @@
+import { Client, DefaultSession, TrackingType, InteractiveSession } from "@sajari/sdk-js";
+  // new SajariSDK.Client("project", "collection")...
+  const websitePipeline = new Client("1627439095818242237", "animal-search").pipeline(
+    "website"
+  );
+
+  const clickTrackedSession = new InteractiveSession(
+    "q",
+    new DefaultSession(TrackingType.Click, "url")
+  );
+
+  const values = { q: "wings" };
+  websitePipeline
+    .search(values, clickTrackedSession.next(values))
+    .then(([response, values]) => {
+      // handle response 
+      response.results.forEach((r) => {
+        console.log(r)
+        const title = document.createElement("a");
+        title.textContent = r.values.title;
+        title.href = r.values.url;
+        title.onmousedown = () => {
+          title.href = r.token.click;
+        };
+
+        document.body.appendChild(title);
+      });
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error)
+    });
+
